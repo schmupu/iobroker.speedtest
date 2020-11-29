79530024a167ec0047e8b62b95826bab42e3ae20
Thorsten Stueben <thorsten.stueben@macbook.local>
2020-11-29 19:52:14 +0100

    Speedtest

---
+++ admin/words.js
@@ -0,0 +1,29 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'speedtest adapter settings': {
+		'en': 'Adapter settings for speedtest',
+		'de': 'Adaptereinstellungen für speedtest',
+		'ru': 'Настройки адаптера для speedtest',
+		'pt': 'Configurações do adaptador para speedtest',
+		'nl': 'Adapterinstellingen voor speedtest',
+		'fr': "Paramètres d'adaptateur pour speedtest",
+		'it': "Impostazioni dell'adattatore per speedtest",
+		'es': 'Ajustes del adaptador para speedtest',
+		'pl': 'Ustawienia adaptera dla speedtest',
+		'zh-cn': 'speedtest的适配器设置'
+	},
+	'polltime': {
+		"en": "Polltime in minute",
+		"de": "Wahlzeit in Minuten",
+		"ru": "Время опроса в минутах",
+		"pt": "Polltime em minuto",
+		"nl": "Polltime in minuten",
+		"fr": "Heure du scrutin en minute",
+		"it": "Polltime in minuti",
+		"es": "Polltime en minutos",
+		"pl": "Czas odpytywania w minutach",
+		"zh-cn": "分钟轮询时间"
+	}
+};
