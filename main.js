dc32b9d24a89d2394ef92f8e43b7445e90446f71
Thorsten Stueben <thorsten@stueben.de>
2020-12-18 07:48:41 +0100

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 /*
  * Created with @iobroker/create-adapter v1.30.1
  */
-
+ 
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
